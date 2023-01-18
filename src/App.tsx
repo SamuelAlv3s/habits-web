eae93764df44114707a1f892ff3c2f6dcf5e3177
Samuel <samuelalves681@gmail>
2023-01-18 20:36:19 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-purple-500 rounded-sm w-8 h-8">Hello World</h1>
+    </>
+  );
+}
+
+export default App;

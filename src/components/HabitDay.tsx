e0a92a91097faa04edc3a9b8e9bc1eec62a7c7ed
Samuel <samuelalves681@gmail>
2023-01-21 14:48:45 -0300

    add checkboxs into popover and modal of habits

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
   amount: number;
@@ -34,6 +36,20 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
           </span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-2 group">
+              <div className="w-8 h-8 rounded-lg flex justify-center items-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
           <Popover.Arrow className="fill-zinc-900" height={8} width={16} />
         </Popover.Content>
       </Popover.Portal>
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-Feira",
+  "Sexta-Feira",
+  "Sábado",
+];
 
 export function NewHabitForm() {
   return (
@@ -18,6 +29,22 @@ export function NewHabitForm() {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeekDays.map((day) => {
+          return (
+            <Checkbox.Root key={day} className="flex items-center gap-2 group">
+              <div className="w-8 h-8 rounded-lg flex justify-center items-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="text-white leading-tight">{day}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
 
       <button
         type="submit"
